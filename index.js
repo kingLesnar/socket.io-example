fe39eacb9bb71e4eb94f5115bc6486a8dbc08ac4
1OneZero <1onenzero0@gmail.com>
2023-07-12 20:14:08 +0530

    socket.io

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.get("/", (req, res) => res.send("Welcome to socket.io"));
 
 io.on("connection", function (socket) {
   console.log("socket connected");
