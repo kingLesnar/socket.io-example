c959321c086c436989eee16215cf661b7338bf09
1OneZero <1onenzero0@gmail.com>
2023-07-12 20:22:37 +0530

    new connection + msg

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const io = require("socket.io")(http);
 
 app.get("/", (req, res) => res.send("Welcome to socket.io"));
 
-io.on("connection", function (socket) {
-  console.log("socket connected");
-});
+// io.on("connection", function (socket) {
+//   console.log("socket connected");
+// });
 
 http.listen(3000, () => console.log("listening on http://localhost:3000"));
 
 io.on("connection", function (socket) {
+  console.log("socket connected");
   io.emit("user connected");
   socket.on("message", function (msg) {
+    console.log("message: " + msg);
     io.emit("message", msg);
   });
 });
