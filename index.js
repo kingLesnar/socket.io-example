61f3ced76fd86606a9408a3e63451f1234a790ee
1OneZero <1onenzero0@gmail.com>
2023-07-17 20:48:25 +0530

    update

--- index.js
+++ index.js
@@ -10,19 +10,99 @@ app.get("/", (req, res) => res.send("Welcome to socket.io"));
 // });
 
 http.listen(3000, () => console.log("listening on http://localhost:3000"));
-let message = "message";
+let message = "GameStarted";
 io.on("connection", function (socket) {
   let players = {};
+  const Questions = {
+    data: [
+      {
+        id: 6549,
+        question:
+          "The planets are kept in motion in their respective orbits by gravitation and centrifugal force",
+        option_1: "TRUE",
+        option_2: "FALSE",
+        option_3: "",
+        option_4: "",
+        answer: "TRUE",
+        content_url: null,
+        import_id: 1,
+        cat_id: 8,
+        type: 2,
+      },
+      {
+        id: 3453,
+        question: "Baahubali star Rana Daggubati has a younger sister",
+        option_1: "",
+        option_2: "",
+        option_3: "TRUE",
+        option_4: "FALSE",
+        answer: "TRUE",
+        content_url: null,
+        import_id: 1,
+        cat_id: 8,
+        type: 2,
+      },
+      {
+        id: 3454,
+        question: "Baahubali star Prabhas is officially married",
+        option_1: "FALSE",
+        option_2: "TRUE",
+        option_3: "",
+        option_4: "",
+        answer: "FALSE",
+        content_url: null,
+        import_id: 1,
+        cat_id: 8,
+        type: 2,
+      },
+      {
+        id: 3455,
+        question:
+          "Salman Khan directed and acted in the quarantine video single Tere Bina",
+        option_1: "TRUE",
+        option_2: "FALSE",
+        option_3: "",
+        option_4: "",
+        answer: "TRUE",
+        content_url: null,
+        import_id: 1,
+        cat_id: 8,
+        type: 2,
+      },
+      {
+        id: 3456,
+        question:
+          "Salman Khan-directed quarantine video single Tere Bina features Katrina Kaif as female lead",
+        option_1: "",
+        option_2: "TRUE",
+        option_3: "FALSE",
+        option_4: "",
+        answer: "FALSE",
+        content_url: null,
+        import_id: 1,
+        cat_id: 8,
+        type: 2,
+      },
+    ],
+  };
+  let score = 0;
   console.log("socket connected .....", socket.id);
   socket.on(message, function (player) {
     players[socket.id] = player;
+    // players[socket.id].Questions = Questions.data;
 
     console.log(
-      "Player " + player.userName + " with id: " + socket.id + " has joined.",
+      "Player " + player.name + " with id: " + socket.id + " has joined.",
     );
-    // console.log("inside chekcing socket.....", socket);
-    // console.log("message: --------- " + player);
-    io.emit("message", player);
+
+    io.emit("message", Questions);
+  });
+
+  socket.on("GameEnded", function (GameEnded) {
+    players[socket.id] = GameEnded;
+
+    score = GameEnded.score;
+    console.log(score);
   });
 
   socket.on("typing", function (message) {
@@ -30,10 +110,12 @@ io.on("connection", function (socket) {
   });
 });
 
-io.on("secondConnection", function (socket) {
-  console.log("second socket connected .....", socket);
-  socket.on("secondMessage", function (msg) {
-    console.log("secondMessage: --------- " + msg);
-    io.emit("secondMessage", msg);
-  });
-});
+// io.on("connection", function (socket) {
+//   console.log("second socket connected .....", socket);
+//   socket.on("secondMessage", function (msg) {
+//     console.log("secondMessage: --------- " + msg);
+//     io.emit("secondMessage", msg);
+//   });
+// });
+
+// --------------------------------------------------------------------
